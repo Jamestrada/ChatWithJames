b1975f2e58f845ec8aaf70a78528aaf1ba103c1e
James Estrada <Jamestrada@users.noreply.github.com>
2020-12-27 23:59:35 -0600

    register user

--- app/src/main/java/com/jamestrada/chatwithjames/RegisterActivity.kt
+++ app/src/main/java/com/jamestrada/chatwithjames/RegisterActivity.kt
@@ -3,9 +3,19 @@ package com.jamestrada.chatwithjames
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var refUsers: DatabaseReference
+    private var firebaseUserId: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -18,6 +28,44 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, WelcomeActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        mAuth = FirebaseAuth.getInstance()
+        register_btn.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val username: String = username_register.text.toString()
+        val email: String = email_register.text.toString()
+        val password: String = password_register.text.toString()
+
+        if (username == "") {
+            Toast.makeText(this, "Please write a username", Toast.LENGTH_LONG).show()
+        } else if (email == "") {
+            Toast.makeText(this, "Please write an email", Toast.LENGTH_LONG).show()
+        } else if (password == "") {
+            Toast.makeText(this, "Please write a password", Toast.LENGTH_LONG).show()
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    firebaseUserId = mAuth.currentUser!!.uid
+                    refUsers = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUserId)
+
+                    val userHashMap = HashMap<String, Any>()
+                    userHashMap["uid"] = firebaseUserId
+                    userHashMap["username"] = username
+//                    userHashMap["profile"] = username
+//                    userHashMap["cover"] = username
+//                    userHashMap["username"] = username
+//                    userHashMap["username"] = username
+//                    userHashMap["username"] = username
+//                    userHashMap["username"] = username
+                } else {
+                    Toast.makeText(this, "Error Message: " + it.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
