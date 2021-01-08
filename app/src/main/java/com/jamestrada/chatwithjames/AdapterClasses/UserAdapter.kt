0b1c1ed9a671692f0701c2fa39dae368d9b301dc
James Estrada <Jamestrada@users.noreply.github.com>
2021-01-07 23:30:29 -0600

    search for users

--- app/src/main/java/com/jamestrada/chatwithjames/AdapterClasses/UserAdapter.kt
+++ app/src/main/java/com/jamestrada/chatwithjames/AdapterClasses/UserAdapter.kt
@@ -1,26 +1,42 @@
 package com.jamestrada.chatwithjames.AdapterClasses
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.jamestrada.chatwithjames.ModelClasses.Users
 import com.jamestrada.chatwithjames.R
+import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.user_search_item_layout.view.*
 
-class UserAdapter(mContext: Context, mUsers: List<Users>, isChatCheck: Boolean) : RecyclerView.Adapter<UserAdapter.ViewHolder?>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+class UserAdapter(mContext: Context, mUsers: List<Users>, isChatCheck: Boolean): RecyclerView.Adapter<UserAdapter.ViewHolder?>() {
+    private val mContext: Context
+    private val mUsers: List<Users>
+    private val isChatCheck: Boolean
 
+    init {
+        this.mUsers = mUsers
+        this.mContext = mContext
+        this.isChatCheck = isChatCheck
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(mContext).inflate(R.layout.user_search_item_layout, viewGroup, false)
+        return UserAdapter.ViewHolder(view)
+    }
 
+    override fun onBindViewHolder(holder: ViewHolder, i: Int) {
+        val user: Users = mUsers[i]
+        holder.userNameTxt.text = user!!.getUsername()
+        Picasso.get().load(user.getProfile()).placeholder(R.drawable.profile).into(holder.profileImageView)
     }
 
     override fun getItemCount(): Int {
-
+        return mUsers.size
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
